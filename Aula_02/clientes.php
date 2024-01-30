adfd00b733a9c5ac9b68f34c28d84d40390a2bab
marcelo <marcelo.vieira@pbh.gov.br>
2024-01-30 11:30:52 -0300

    atualizações da AULA_02

--- Aula_02/clientes.php
+++ Aula_02/clientes.php
@@ -1,8 +1,9 @@
 <?php
 	// conectando o PHP - com o sqlite, usando o PDO
 
-	$db = new PDO("sqlite:supertech.db");
+	// êêêêêêêêêêêêêêêêêêêêê nossa primeira API
 
+	$db = new PDO("sqlite:supertech.db");
 
 	function inserirDadosNaTabelaCliente($db,$cod_cliente,$nome,$email,$telefone,$endereco)
 	{
@@ -11,7 +12,6 @@
 	// este comando SQL irá inserir dados na nossa tabela
 
 	$sql = "INSERT INTO clientes (cod_cliente,nome,email,telefone,endereco) values (:cod_cliente,:nome,:email,:telefone,:endereco)";
-
 	
 	$stmt = $db->prepare($sql);
 
@@ -37,9 +37,53 @@
 		
 	}
 
+	function buscaCliente($db,$cod_cliente = null){
+		$sql = "SELECT * FROM clientes WHERE cod_cliente = :cod_cliente";
+
+		$stmt = $db->prepare($sql);
+
+		$stmt->bindValue(":cod_cliente", $cod_cliente);
+		
+		$stmt->execute();
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);		
+	}
+	
+	function alteraCliente($db,$cod_cliente,$nome,$email,$telefone,$endereco){
+		$sql = "UPDATE clientes SET nome = :nome, email = :email, telefone = :telefone, endereco = :endereco WHERE cod_cliente = :cod_cliente";
+
+		$stmt = $db->prepare($sql);
+
+		$stmt->bindValue(":cod_cliente", $cod_cliente);
+		$stmt->bindValue(":nome", $nome);
+		$stmt->bindValue(":email", $email);
+		$stmt->bindValue(":telefone", $telefone);
+		$stmt->bindValue(":endereco", $endereco);
+
+		$stmt->execute(); 
+	}
+
+	function excluiCliente($instanciaBD, $codigo){
+		$sql = "DELETE FROM clientes WHERE cod_cliente = :cod_cliente";
+
+		$stmt = $instanciaBD->prepare($sql);
+
+		$stmt->bindValue(":cod_cliente", $codigo);
+		
+		$stmt->execute();
+	}
+	
 	//inserirDadosNaTabelaCliente($db, 21,"Maria","mariinha@gmail.com","3177777","rua das gaivotas,29");
 
-	print_r buscaTodosClientes($db);
+	//print_r(buscaTodosClientes($db));
+
+	//alteraCliente($db,1,"José das couves","ze@hotmail.com","215555555","rua dos ladrões,171");
+
+	//excluiCliente($db, 1);
+
+	//print_r(buscaTodosClientes($db));		
 	
+	//$resultadoBusca = buscaCliente($db,1);
+	//print_r($resultadoBusca);	
 ?>
 
