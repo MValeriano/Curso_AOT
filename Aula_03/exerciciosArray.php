9cab01edd63f67341ca1a19141cfded02522fc15
marcelo <marcelo.vieira@pbh.gov.br>
2024-02-19 10:14:30 -0300

    Subindo Aula_02

---
+++ Aula_03/exerciciosArray.php
@@ -0,0 +1,44 @@
+<?php
+/*
+1. Criar um Array Simples:
+
+Declare um array simples com 5 elementos e imprima seu conteúdo.
+
+2. Acessar Elementos do Array:
+
+Acesse e imprima o terceiro elemento do array criado no exercício anterior.
+
+3. Adicionar Elementos:
+
+Adicione mais dois elementos ao array e imprima o array atualizado.
+
+4. Contar Elementos:
+
+Conte e imprima o número total de elementos no array.
+
+5. Remover Elemento:
+
+Remova o segundo elemento do array e imprima o array resultante.
+
+6. Array Associativo:
+
+Crie um array associativo com informações sobre uma pessoa (nome, idade, cidade, etc.) e imprima seus valores.
+
+7. Modificar Valor:
+
+Modifique a idade da pessoa no array associativo para um novo valor e imprima o array atualizado.
+
+8. Mesclar Arrays:
+
+Crie um segundo array associativo e mescle-o com o array criado no exercício 6. Imprima o array resultante.
+
+9. Ordenar Array:
+
+Crie um array com números desordenados e use a função de ordenação para ordená-lo de forma ascendente. Imprima o array ordenado.
+
+10. Filtrar Array:
+
+Crie um array com números mistos (inteiros e decimais) e use uma função para filtrar apenas os valores inteiros. Imprima o array resultante.
+*/
+
+?>
