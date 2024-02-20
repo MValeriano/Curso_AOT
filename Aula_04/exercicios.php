55104e205e200d29cf5c8d857dcc8817b6eb7571
marcelo <marcelo.vieira@pbh.gov.br>
2024-02-20 11:44:57 -0300

    Aula_04

---
+++ Aula_04/exercicios.php
@@ -0,0 +1,30 @@
+<?php 
+	// Operadores aritméticos
+	// Exercício 1 :
+	// Dados dois números $a = 10 e $b = 20. Calcule a soma, a subtração, a multiplicação e a 	divisão.
+
+	// Exercício 2 :
+	// Dado um número $x = 15. incremente e decremente $x.
+
+	// Operadores de atribuição
+	// Exercício 3: 
+	// Dado um número $y = 30. Use operadores de atribuiição para adicionar 10, subtrair 5, multiplicar por 2 e dividir por 3.
+
+	// Exercício 4:
+	// Dado um número $z = 50. Use o operador de aribuição para concatenar a string "50".
+
+	// Operadores de comparação
+	// Exercício 5
+	// Dados dois números $m = 100 e $n = 200. Verifique se $m é igual, menor, maior, menor ou igual, maior ou igual a $n.
+
+	// Exercício 6:
+	// Dados dois valores $o = "100" e $p = 100. verifique se $o é identico e não identico a $p.
+	// Operadores lógicos
+	// Exercício 7:
+	// Dados três valores booleanos $bool1 = true, $bool2 = false e $bool3 = true. verifique a conjunção ( && ou and ), a disjunção ( || ou or ) entre eles.
+
+	// Exercício 8:
+	// Dado um valor booleano $boll4 = false. verifique a negação ( ! ou not ) de $bool4.
+ 
+	
+?>
---
+++ Aula_04/revisao.php
@@ -0,0 +1,65 @@
+<?php
+
+	// Variavéis 
+	// Guardar e recuperar nossos dados, que serão usados quando o sistema estiver funcionando.
+	
+	// Em PHP a variavel sempre começa com o "$" depois o identificador ( $identificador )
+	// tipos de dados em PHP
+	// bool : valores booleanos, verdadeiro e falso;
+	// int : números inteiros, sem o ( . ) sem decimais;
+	// float : número de ponto flutuante ( decimais )  com ( . );
+	// string : sequência de caracteres;
+	// array : matriz;
+	// object : objeto;
+	// null: não possui um valor atribuído.
+
+	// para atribuir um valor à váriavel usamos o operador de atribuição ( = )
+	// então o valor à esquerda do operador de atribuição recebe o valor à direita do operador de atribuição
+	// $identificador = valor;
+	// ------------------------------------------------------------------------
+	
+	// Métodos para exibir informações na tela 
+
+	// echo = imprimir somente strings;
+	// print = imprimir somente strings;
+	// print_r = que irá exibir arrays;
+	// var_dump = que irá exibir os dados na tela com mais informações.
+
+	//------------------------------------------------------------------------
+
+
+	// Operadores 
+	// Operadores de atribuição 
+	// =
+
+	// Operadores aritméticos 
+	// + (soma), - (subtração), / (divisão) , * (multiplicação), % (resto), ** (exponencial)
+
+	// Operadores de comparação 
+	// < ( Menor ) , > ( maior ), <= ( Menor ou igual ), >= ( maior ou igual )
+	// == ( igual ), != ( diferente ), ! ( negar ), ( (===), (!==) exatamente iguais, além do valor o tipo tem que ser igual)
+	// elemento à esquerda ( operador ) elemento à direita
+
+	// Operadores lógicos
+	// && ou and = "E", 
+	// || ou or = "OU",
+	/*
+	var_dump(true && true); 
+	echo "<br/>";
+	var_dump(false && true); 
+	echo "<br/>";
+	var_dump(false && false); 
+	echo "<br/>";
+	var_dump(true || true); 
+	echo "<br/>";
+	var_dump(false || true); 		
+	echo "<br/>";
+	var_dump(false || false); 	
+	echo "<br/>";
+	var_dump( (false && false) || (true || false) );
+	*/
+	// Operadores binários
+	// ++ (incremento), -- (decremento)
+	// incrementa ou decremento o valor em uma unidade
+
+?>
