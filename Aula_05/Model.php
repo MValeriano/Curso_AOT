fb7fd8ff1a82dd70fc6f9424a47f83a033aa2b31
Marcelo Valeriano <marcelovaleriano@hotmail.com>
2025-10-23 15:51:13 -0300

    Refactor Model methods to accept parameters

--- Aula_05/Model.php
+++ Aula_05/Model.php
@@ -2,7 +2,6 @@
 
 class Model
 {
-
     // $variavel -> atributos
     // modificadores de acesso
     // public
@@ -24,40 +23,64 @@ class Model
       $this->conn = new PDO('sqlite:'.$banco);
     }
 
-    public function create(){
-
+    public function create($data){
+        // Filtra os dados permitidos usando fillables
+        $filteredData = array_intersect_key($data, array_flip($this->fillables));
+        
+        $columns = implode(', ', array_keys($filteredData));
+        $placeholders = ':' . implode(', :', array_keys($filteredData));
+        
+        $sql = "INSERT INTO {$this->table} ({$columns}) VALUES ({$placeholders})";
+        $stmt = $this->conn->prepare($sql);
+        
+        foreach($filteredData as $key => $value){
+            $stmt->bindValue(":{$key}", $value);
+        }
+        
+        return $stmt->execute();
     }
 
     public function all(){
       $sql = "SELECT * FROM " . $this->table;
-
       $stmt = $this->conn->prepare($sql);
-
       $stmt->execute();
-
       return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function update(){
-
+    public function update($id, $data){
+        // Filtra os dados permitidos usando fillables
+        $filteredData = array_intersect_key($data, array_flip($this->fillables));
+        
+        $setClause = [];
+        foreach(array_keys($filteredData) as $column){
+            $setClause[] = "{$column} = :{$column}";
+        }
+        $setClause = implode(', ', $setClause);
+        
+        $sql = "UPDATE {$this->table} SET {$setClause} WHERE {$this->primaryKey} = :id";
+        $stmt = $this->conn->prepare($sql);
+        
+        foreach($filteredData as $key => $value){
+            $stmt->bindValue(":{$key}", $value);
+        }
+        $stmt->bindValue(':id', $id);
+        
+        return $stmt->execute();
     }
 
     public function get($id){
       $sql = "SELECT * FROM " . $this->table . " WHERE " . $this->primaryKey . " = :id ";
-
       $stmt = $this->conn->prepare($sql);
-
       $stmt->bindValue(":id", $id);
-
       $stmt->execute();
-      
       return $stmt->fetch(PDO::FETCH_ASSOC);      
     }
 
-    public function destroy(){
-
+    public function destroy($id){
+        $sql = "DELETE FROM {$this->table} WHERE {$this->primaryKey} = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        return $stmt->execute();
     }
 
 }
-
-
